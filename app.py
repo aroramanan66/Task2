46ed7e9f7dd1b23096cfb874917919e43afa3acd
aroramanan66 <92618957+aroramanan66@users.noreply.github.com>
2021-10-27 22:31:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,52 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+ 
+app = Flask(__name__)
+ 
+UPLOAD_FOLDER = 'C:/Users/arora/Downloads/Project3/static/uploads/'
+ 
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif','JPG'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-app=Flask(__name__)
-#Rendering to the index.html template  
-@app.route("/")
+#Rendering to the index.html template    
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+ 
 
-#Performing the GET,POST operation 
-@app.route("/login",  methods=["POST","GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        password1 = request.form["password"]
-        file1=request.form["file"]
-        return redirect(url_for("user",usr=user,passw=password1,fil=file1))
+#Uploading the file(image) 
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
     else:
-        return render_template("login.html")
+        flash('Allowed image types are - png, jpg, jpeg, gif,JPG')
+        return redirect(request.url)
 
-#Display the values enter in the POST operation
-@app.route("/<usr>/<passw>/<fil>")
-def user(usr,passw,fil):
-    return f"<h1> Name: {usr}</h1> <h2> Password: {passw}</h2> Image: {fil}"
-
+#Displaying the file(image) 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+ 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run() 
